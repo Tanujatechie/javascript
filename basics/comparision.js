93a70a13a56db16ad42a3cb4c9693ec235ef1fa7
Tanuja Poddhuturi <134959007+Tanujatechie@users.noreply.github.com>
2024-06-23 15:45:18 +0000

    comparision

---
+++ basics/comparision.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 < 2);
+// console.log(2>= 5);
+// console.log(2<=5);
+// console.log(2 ==3);
+// console.log(2 !=4);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0 );//ture bcoz 
+//there is diff btw equality == and comparision >=, <=... 
+// comparisions converts null to a number treating as 0 
+// thats why null >= 0 bcome true 
+
+console.log(undefined ==0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// comparision and equality check is dofferent mechanisms in JS
+
+
+//  strict check -> ===
+// not only checks the value it also checks the dtype of the value 
+console.log("2" ===2);
+
+// only clean code is the main thing which is most important to us 
