901a1e44dd2d133d89715c12f6563d657ae4a59e
Tanuja Poddhuturi <134959007+Tanujatechie@users.noreply.github.com>
2024-06-23 10:20:37 +0000

    varaibles

---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const accountId = 144567 
+let accountEmail = "Tanujapoddhuturi2003@gmail.com"
+var accountPassword = "123456"
+accountCity = "Jaipur"
+let accountState; 
+
+console.table([accountId , accountEmail , accountPassword , accountCity, accountState]);
