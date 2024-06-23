1b2582cb2c46249d5e1e9cbf58b539d85ff9af43
Tanuja Poddhuturi <134959007+Tanujatechie@users.noreply.github.com>
2024-06-23 10:37:28 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version 
+
+/*alert("Hello")// we are using node js not browser so it doesn't work here */
+
+console.log(3+3); console.log("Tanuja") // code readability must be high 
+
+// documentation for the Javascript is ECMA script - tc39.es
+// these are for knowing the standards 
+
+
+//DataTypes - primitive data types 
+// there are very few dtypes 
+ let name ="Tanuja"
+ let age = 21
+ let loggedIn = false
+ //number => 2^53
+ //bigInt - mostly used in stock etc
+ //string - "" / ''
+ //boolean - false / true 
+ //null - standalone value (it is a representation of empty value but not undefined)
+ //undefined - value is not defined but declared the varaibale 
+ //symbol - unique
+
+ //object 
+
+ console.log(typeof age);
+ console.log(typeof null);// null is a object 
+ console.log(typeof undefined);
