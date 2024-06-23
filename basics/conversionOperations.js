db50218e3db2b55ab3977ba2d0961e70302e1ad4
Tanuja Poddhuturi <134959007+Tanujatechie@users.noreply.github.com>
2024-06-23 11:14:35 +0000

    operations

--- basics/conversionOperations.js
+++ basics/conversionOperations.js
@@ -1,25 +1,67 @@
-let score = "Tanuja"
+// let score = "Tanuja"
 
 
-console.log(typeof score);//number 
+// console.log(typeof score);//number 
 
-let valInNumber = Number(score)
-console.log(typeof valInNumber)
-console.log(valInNumber);//NaN
-// "33" => 33 
-// "33abc" => NaN
-// ture => 1 
-//false => 0
-//string =>NaN 
+// let valInNumber = Number(score)
+// console.log(typeof valInNumber)
+// console.log(valInNumber);//NaN
+// // "33" => 33 
+// // "33abc" => NaN
+// // ture => 1 
+// //false => 0
+// //string =>NaN 
 
-let isLoggedIn = ""
+// let isLoggedIn = ""
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
-// 1=> true , 0=> false
-// "" =>false , "tanuja" => true 
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// // 1=> true , 0=> false
+// // "" =>false , "tanuja" => true 
 
-let someNumber = 33 
+// let someNumber = 33 
 
-let stringNumer = String(someNumber)
-console.log(typeof stringNumer)
+// let stringNumer = String(someNumber)
+// console.log(typeof stringNumer)
+
+//*****************OPERATIONS*****************
+let value = 3
+let negValue = -value 
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2)
+// console.log(2*3);
+// console.log(2**8);
+// console.log(8%4)
+// console.log(8/4)
+
+
+let str1 = "Hello "
+let str2 = "tanuja"
+
+let str3 = str1 + str2
+console.log(str3);
+
+
+
+//complexity in conversion of string to number 
+// console.log("1"+ 2)
+// console.log(1+ "2")
+// console.log("1"+"2")
+// console.log("1"+2+2)
+// console.log(2+1+"2")
+
+console.log(+true);//1
+console.log(+"");//0 // don't do like this 
+
+//Assignment operator 
+
+let num1 , num2 , num3 
+
+num1 = num2 = num3 = 2+2 // only focus on readability and don't use this 
+
+let gameCounter = 100
+// gameCounter ++ ; 
+++gameCounter;
+console.log(gameCounter);
