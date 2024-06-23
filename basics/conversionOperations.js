0c1babfe6beb08564602cb85f6e5907ae1ad2918
Tanuja Poddhuturi <134959007+Tanujatechie@users.noreply.github.com>
2024-06-23 10:51:53 +0000

    conversion of dtypes

---
+++ basics/conversionOperations.js
@@ -0,0 +1,25 @@
+let score = "Tanuja"
+
+
+console.log(typeof score);//number 
+
+let valInNumber = Number(score)
+console.log(typeof valInNumber)
+console.log(valInNumber);//NaN
+// "33" => 33 
+// "33abc" => NaN
+// ture => 1 
+//false => 0
+//string =>NaN 
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+// 1=> true , 0=> false
+// "" =>false , "tanuja" => true 
+
+let someNumber = 33 
+
+let stringNumer = String(someNumber)
+console.log(typeof stringNumer)
